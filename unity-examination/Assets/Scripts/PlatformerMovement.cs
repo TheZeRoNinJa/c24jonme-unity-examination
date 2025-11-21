2dd6e1031e6707f2bb60ecbe9a1ee96c7e496141
TheZeRoNinJa <45365623+TheZeRoNinJa@users.noreply.github.com>
2025-11-21 14:45:40 +0100

    Växla mellan flera animationer (5p)
    Kan vara lite svårt att se, men spelaren byter mellan en konstig idle animation och en walk animation när den går/står still.

--- unity-examination/Assets/Scripts/PlatformerMovement.cs
+++ unity-examination/Assets/Scripts/PlatformerMovement.cs
@@ -94,6 +94,7 @@ public class PlatformerMovement : MonoBehaviour
         rb.linearVelocity = velocity;
         
         // Write movement animation code here. (Suggestion: send your current velocity into the Animator for both the x- and y-axis.)
+        animator.SetFloat("speed", rb.linearVelocity.x);
     }
 
     private bool IsGrounded()
